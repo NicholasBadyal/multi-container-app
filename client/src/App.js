d29c34c943fbf203bb203361fff34c6e951fa7b8
Nicholas Badyal <nicholasbadyal@outlook.com>
2021-02-28 01:20:45 -0800

    Created react frontend app

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './otherpage';
+import Fib from './fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        <div className="App-subheader">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          </div>
+          <div className="App-Links">
+          <Link className="Link" to="/">Home</Link>
+          <Link className="Link" to="/otherpage">Other Page</Link>
+          </div>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
